3cae0fe2aeed8e788a37a84151c9012a5da25e5c
tobiashuelden <tobias.huelden@chicagobooth.edu>
2021-12-08 01:20:07 +0000

    Fixed db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+csv_text = File.read(Rails.root.join("lib", "csvs", "companies.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+csv.each do |row|
+  c = Company.new
+  c.name = row["name"]
+  c.business_area = row["business_area"]
+  c.save
+end
+
+csv_text = File.read(Rails.root.join("lib", "csvs", "chatrooms.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+csv.each do |row|
+  c = Chatroom.new
+  c.company_id = row["company_id"]
+  c.save
+end
+
